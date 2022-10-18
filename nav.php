eb380afce3a1c1cc00dcdf7eeeb70523d640d3d5
Ismail Kassim <ikassim9@gmail.com>
2022-10-17 19:48:21 -0500

    enhanced the ui

---
+++ nav.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hangman</title>
+    <link rel="stylesheet" href="./css/mainStylesheet.css">
+</head>
+
+<body>
+
+
+    <nav>
+
+        <ul>
+            <a href="index.php">
+                <li class="logo">
+                    <img src="./images/app_logo.png" alt="">
+                    <br>
+                    Hangman
+                </li>
+
+            </a>
+
+
+            <a href="index.php">
+                <li class="hangman-logo">
+                    <img src="./images/home.png" alt="">
+                    <br>
+                    Home
+                </li>
+
+            </a>
+
+            <a href="index.php">
+                <li>
+                    <img src="./images/login.png" alt="">
+                    <br>
+                    Login
+                </li>
+
+            </a>
+
+        </ul>
+    </nav>
+
+    <div class='play-btn'>
+    <a class="btn" href="hangman.php">Start game</a>
+
+    </div>
+
+</body>
+
+</html>
